7abdd4a47b4d843427607166c60aa8f0e1fb2c89
Min Thu Aung <reoknite@gmail.com>
2023-03-16 23:05:42 -0500

    Use absolute imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 
-import Container from '../components/container'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import MoreStories from '../components/more-stories'
-import Post from '../interfaces/post'
-import {getAllPosts} from '../lib/api'
-import {CMS_NAME} from '../lib/constants'
+import Container from 'components/container'
+import HeroPost from 'components/hero-post'
+import Intro from 'components/intro'
+import Layout from 'components/layout'
+import MoreStories from 'components/more-stories'
+import Post from 'interfaces/post'
+import {getAllPosts} from 'lib/api'
+import {CMS_NAME} from 'lib/constants'
 
 type Props = {
   allPosts: Post[]
